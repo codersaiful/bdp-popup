13d9c884fcd5b95c39e0528e2c555b13d8a12c62
Saiful Islam <codersaiful@gmail.com>
2023-08-20 10:26:32 +0600

    done

---
+++ admin/page/universal-settings.php
@@ -0,0 +1,56 @@
+<table class="wcmmq-table universal-setting">
+    <thead>
+        <tr>
+            <th class="wcmmq-inside">
+                <div class="wcmmq-table-header-inside">
+                    <h3><?php echo esc_html__( 'Settings (Universal)', 'wcmmq' ); ?></h3>
+                </div>
+                
+            </th>
+            <th>
+            <div class="wcmmq-table-header-right-side"></div>
+            </th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+<!--     
+        <tr>
+            <td>
+                <div class="wcmmq-form-control">
+                    <div class="form-label col-lg-6">
+                        LabelTagHere
+                    </div>
+                    <div class="form-field col-lg-6">
+                        InputFieldOrAnyOtherField
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="wcmmq-form-info">
+                    DescriptionOfField_and_docLink
+                </div> 
+            </td>
+        </tr> 
+-->
+
+        <tr>
+            <td>
+                <div class="wcmmq-form-control">
+                    <div class="form-label col-lg-6">
+                        Title
+                    </div>
+                    <div class="form-field col-lg-6">
+                        <input name="data[title]" id="data[title]" class="ua_input_number" value=""  type="number" step=any>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="wcmmq-form-info">
+                    DescriptionOfField_and_docLink
+                </div> 
+            </td>
+        </tr> 
+    </tbody>
+</table>
